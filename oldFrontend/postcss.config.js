4583e2be54bb4794f7a3ae8f65557bb66e02351e
Michael Minelli <git@minelli.swiss>
2025-05-19 19:29:19 +0200

    New frontend base

--- oldFrontend/postcss.config.js
+++ oldFrontend/postcss.config.js
@@ -1,11 +1,11 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
+const purgecss = require('@fullhuman/postcss-purgecss').purgeCSSPlugin({
   content: ['./src/**/*.js', './src/**/*.jsx', './public/**/*.html'],
   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 });
 
 module.exports = {
   plugins: [
-    require('tailwindcss'),
+    require('@tailwindcss/postcss'),
     ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 };
