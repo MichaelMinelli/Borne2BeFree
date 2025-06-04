c18a6ed2fc52304f27e5434153ac02cbee446c5d
Michael Minelli <git@minelli.swiss>
2025-06-04 15:43:57 +0200

    Alertbox => adapt code to receive style infos

--- frontend/src/components/AlertBox.tsx
+++ frontend/src/components/AlertBox.tsx
@@ -1,22 +1,26 @@
-import { ExclamationSolidIcon } from './Icons';
+import { ExclamationSolidIcon, InformationSolidIcon } from './Icons';
 
 import './AlertBox.css';
-import React                    from 'react';
+import React                                          from 'react';
+import type { Alert }                                 from '../types/Alert.ts';
 
 
 interface AlertBoxProps {
     children: React.ReactNode;
     visible: boolean;
+    params: Alert;
 }
 
 
 const AlertBox: React.FC<AlertBoxProps> = ({
                                                children,
-                                               visible
+                                               visible,
+                                               params
                                            }) => {
-    return <div className={ `flex flex-row items-center m-4 bg-red-300 rounded-lg border-2 border-red-800 p-2 ${ visible ? '--show-alert' : '--hide-alert' }` }>
-        <ExclamationSolidIcon classes="w-6 h-6 text-red-800" />
-        <div className="mx-2 text-red-900">
+    return <div className={ `flex flex-row items-center m-4 bg-${ params.color }-50 rounded-lg border-2 border-${ params.color }-800 p-2 ${ visible ? '--show-alert' : '--hide-alert' }` }>
+        { params.isWarning && <ExclamationSolidIcon classes={ `w-6 h-6 text-${ params.color }-800` } /> }
+        { !params.isWarning && <InformationSolidIcon classes={ `w-6 h-6 text-${ params.color }-800` } /> }
+        <div className={ `mx-2 text-${ params.color }-900` }>
             { children }
         </div>
     </div>;
