f8fb1d418688bea04f022beb3c39b41b097c62b1
Michael Minelli <git@minelli.swiss>
2025-06-05 12:29:01 +0200

    Backend => Load config and api key from files

--- backend/src/config/Config.ts
+++ backend/src/config/Config.ts
@@ -1,12 +1,15 @@
 import * as process    from 'process';
-import userConfigFile  from '../config.js';
 import { Library }     from '../types/Library';
 import { Zone }        from '../types/Zone';
 import { UserLibrary } from '../types/UserLibrary';
+import * as fs         from 'fs-extra';
+import { UserConfig }  from '../types/UserConfig';
 
 
 class Config {
     public readonly production: boolean;
+
+    public readonly userConfigFilePath: string;
 
     public readonly logsFolder: string;
 
@@ -14,17 +17,23 @@ class Config {
         port: number
     };
 
-    public readonly libraries: Array<Library>;
+    public libraries: Array<Library> = [];
     public allIpSet: Set<string> = new Set<string>();
 
     constructor() {
         this.production = process.env.NODE_ENV === 'production';
+
+        this.userConfigFilePath = process.env.USER_CONFIG_FILE_PATH ?? `${ __dirname }/../config.json`;
 
         this.logsFolder = process.env.LOGS_FOLDER ?? 'logs';
 
         this.api = {
             port: Number(process.env.API_PORT || 30992)
         };
+    }
+
+    async loadUserConfig() {
+        const userConfigFile: UserConfig = await fs.readJson(this.userConfigFilePath) as UserConfig;
 
         this.libraries = userConfigFile.zones.flatMap((zone: Zone) => {
             return zone.libraries.map((library: UserLibrary) => {
@@ -33,6 +42,15 @@ class Config {
                 };
             });
         });
+
+        this.libraries = this.libraries.map((library: Library) => {
+            // If library.apiKey is not a path (contain no '/'), assume that it is the name of a Docker secret
+            const path = library.apiKey.includes('/') ? library.apiKey : `/run/secrets/${ library.apiKey }`;
+            library.apiKey = fs.readFileSync(path, 'utf8');
+            return library;
+        });
+
+        console.log('Loaded user config:', this.libraries);
     }
 }
 
