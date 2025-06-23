b801092edbf5b54f8d7d15bed939722390fb44ce
Michael Minelli <git@minelli.swiss>
2025-06-23 14:14:32 +0200

    Backend => verify double apiName

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,18 +12,18 @@ import logger         from './logging/WinstonLogger.js';
 (async () => {
     await Config.loadUserConfig(logger);
 
-    // ensure that there is no duplicate library codes
+    // ensure that there is no duplicate library apiName
     {
-        const allLibraryCodeArray: string[] = Config.libraries.flatMap((l: Library) => l.code);
+        const allLibraryCodeArray: string[] = Object.values(Config.libraries).flatMap((l: Library) => l.apiName);
 
         if ( allLibraryCodeArray.length !== (new Set(allLibraryCodeArray)).size ) {
-            throw new Error('Multiple libraries are configured with the same code but that\'s not allowed');
+            throw new Error('Multiple libraries are configured with the same apiName but that\'s not allowed');
         }
     }
 
     // ensure that circ desks have different names
     {
-        const circDeskAndLibraryNames_array: string[] = Config.libraries.map((l: Library) => l.apiName + '_' + l.apiCircDesk);
+        const circDeskAndLibraryNames_array: string[] = Object.values(Config.libraries).map((l: Library) => l.apiName + '_' + l.apiCircDesk);
         const circDeskAndLibraryNames_set = new Set(circDeskAndLibraryNames_array);
         if ( circDeskAndLibraryNames_array.length !== circDeskAndLibraryNames_set.size ) {
             throw new Error('Multiple libraries are configured with the same name but that\'s not allowed');
