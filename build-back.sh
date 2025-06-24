fc569e3e86fd90bfb01576fd6ab428938895f957
Eric Pascal <eric@pascal-lab.ch>
2025-06-24 11:27:41 +0200

    split build configs

---
+++ build-back.sh
@@ -0,0 +1,2 @@
+docker buildx build -t borneo-backend:latest -f ./Dockerfile_Backend .
+docker service update --force borneo_borneo-backend
--- build-front.sh
+++ build-front.sh
@@ -1,4 +1,2 @@
-docker buildx build -t borneo-backend:latest -f ./Dockerfile_Backend .
 docker buildx build -t borneo-frontend:latest -f ./Dockerfile_Frontend .
 docker service update --force borneo_borneo-frontend
-docker service update --force borneo_borneo-backend
